d43acfa4602f169a02cd92543db1d682b27ad4c1
leenduati <leenduati21@gmail.com>
2020-09-07 17:18:56 +0300

    My Projects page has been added

--- dist/JS/main.js
+++ dist/JS/main.js
@@ -30,3 +30,32 @@ function toggleMenu() {
 
     }
 }
+
+const IMAGE = document.querySelector(".image");
+const PREV = document.querySelector("#prev");
+const NEXT = document.querySelector("#next");
+let IMAGEARRAY = ["./Images/batman.jpg", "./Images/boondocks.jpg", "./Images/depressedgirl.jpg", "./Images/celeb.jpg", "./Images/eddeddeddy.jpg", "./Images/godofwar.jpg", "./Images/friend.jpg", "./Images/friend2.jpg", "./Images/friend3.jpg", "./Images/theflash.jpg", "./Images/theEye.jpg"];
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var index;
+    var x = document.getElementsByClassName("image");
+
+    if (n > x.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = 1;
+    }
+    for (index = 0; index < x.length; index++) {
+        x[index].style.display = "none";
+
+    }
+    x[slideIndex - 1].style.display = "block";
+    x[slideIndex - 1].classList.add("image")
+}
